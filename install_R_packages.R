3e4e1293f0b97a378c46ab87a3c81214fc67158f
weinstockj <josh82093@gmail.com>
2016-02-24 13:45:13 -0500

    Create install_R_packages.R

---
+++ install_R_packages.R
@@ -0,0 +1,15 @@
+PACKAGES = c("dplyr", 
+            "ggplot2", 
+            "tidyr", 
+            "magrittr", 
+            "RMySQL", 
+            "caret",
+            "lme4"
+            "rstan",
+            "gbm",
+            "reshape2",
+            "readr",
+            "Rcpp",
+            "plyr")
+
+install.packages(PACKAGES, repos='http://cran.rstudio.com')
