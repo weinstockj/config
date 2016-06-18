97528d8fd68578b70137905f90341844cc8698e0
weinstockj <josh82093@gmail.com>
2016-06-18 00:21:46 -0400

    add a couple other small details to plugin script

--- add_vim_plugins.sh
+++ add_vim_plugins.sh
@@ -1,9 +1,13 @@
+#install pathogen
 mkdir -p ~/.vim/autoload ~/.vim/bundle && \
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
-cd ~/.vim/bundle
+# download plugins
+mkdir ~/.vim/bundle && cd ~/.vim/bundle
 git clone https://github.com/scrooloose/nerdtree
 git clone https://github.com/bling/vim-airline
 git clone https://github.com/altercation/vim-colors-solarized
 git clone https://github.com/scrooloose/syntastic
 
+# copy .vimrc in this repo to the user ~/.vimrc
+cp .vimrc ~/.vimrc
