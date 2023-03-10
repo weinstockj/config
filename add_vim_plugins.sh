a822dfa4e9fc4fe80a639e7f8d98322d1eb01fa5
Josh Weinstock <jweinstk@stanford.edu>
2023-03-10 14:13:37 -0800

    adding neovim config

--- add_vim_plugins.sh
+++ add_vim_plugins.sh
@@ -1,3 +1,4 @@
+wd=$(pwd)
 #install pathogen
 mkdir -p ~/.vim/autoload ~/.vim/bundle && \
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
@@ -6,5 +7,6 @@ curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 mkdir ~/.vim/bundle 
 source clone_plugins.sh
 
+cd $wd
 # copy .vimrc in this repo to the user ~/.vimrc
 cp .vimrc ~/.vimrc
