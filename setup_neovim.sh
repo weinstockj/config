5e2a11f2afc24027a30937ff3f8a04902fe40729
joshua.s.weinstock@gmail.com <Josh Weinstock>
2024-12-25 16:32:01 -0500

    neovim config updates and remove unused files

--- setup_neovim.sh
+++ setup_neovim.sh
@@ -1,5 +1,7 @@
 mkdir -p ~/.config/nvim
+mkdir -p ~/.config/nvim/lua
 cp init.vim ~/.config/nvim
+cp init.lua ~/.config/nvim/lua
 
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
