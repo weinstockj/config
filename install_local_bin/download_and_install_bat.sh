d83c78f54d1ea438a8195d49715e33b007dc09ca
joshua.s.weinstock@gmail.com <Josh Weinstock>
2025-01-14 15:10:13 -0500

    update bat

--- install_local_bin/download_and_install_bat.sh
+++ install_local_bin/download_and_install_bat.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-version=v0.22.1
+version=v0.25.0
 wget https://github.com/sharkdp/bat/releases/download/$version/bat-$version-x86_64-unknown-linux-musl.tar.gz
 
 tar -xzvf bat-$version-x86_64-unknown-linux-musl.tar.gz
