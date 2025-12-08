ff7b655c051608b2981e4de126f3da1e3a7c40c3
Josh Weinstock <josh.weinstock@emory.edu>
2025-12-08 10:19:34 -0500

    comment out init.vim file

--- setup_neovim.sh
+++ setup_neovim.sh
@@ -1,6 +1,6 @@
 mkdir -p ~/.config/nvim
 mkdir -p ~/.config/nvim/lua
-cp init.vim ~/.config/nvim
+# cp init.vim ~/.config/nvim
 cp init.lua ~/.config/nvim/lua
 
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
